da8796a10073b04e898706308521b08787a35b13
CarlosSoares00 <carlossoarespedro20@gmail.com>
2024-04-12 22:53:49 +0100

    [Backend] Adicionando o backend com o servidor rodando em padrao

---
+++ backend/src/App.js
@@ -0,0 +1,13 @@
+// Arquivo principal do Sistema ou aplicativo
+
+const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+
+module.exports = app
---
+++ backend/src/index.js
@@ -0,0 +1,11 @@
+// Arquivo responsavel pelo Servidor
+
+const app = require('./App')
+const port = 8000
+
+const server = app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
+
+
+module.exports = server
