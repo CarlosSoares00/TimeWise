047b9ede26acc9abd08d4251437c1eaa8c3e504c
CarlosSoares00 <carlossoarespedro20@gmail.com>
2024-04-20 22:05:17 +0100

    [Backend] Adicionando Middleware para armazenamento de audio actualizado.

--- backend/src/middlewares/uploadMusicMiddlewares.js
+++ backend/src/middlewares/uploadMusicMiddlewares.js
@@ -1,30 +1,15 @@
 const multer = require("multer")
-const path = require('path')
 
 const storage = multer.diskStorage({ // Objecto que determina como os arquivos enviados serão armazenados no disco.
   
-  distanation: function (req, file, cb) {
-    if(file.fieldname === 'audio') {
-      cb(null, path.join('uploads', 'music'))
-    } else {
-      cb(new Erro ('Campo de arquivo nao reconhecido.'))
-    }
-  }, //Responsável por determinar onde os arquivos serão salvos, com base no nome do campo do arquivo
-
-  filename: function(req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/music/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
   }
 })
 
-// Middleware de upload
-const upload = multer({ 
-  storage: storage, // Configuração do armazenamento de arquivos,
-  limits: {fileSize: 1024 * 1024 * 20}// Definindo o Tamanho máximo permitido para cada arquivo enviado (20)
-})
-
-/*
-configurando um middleware de upload de arquivos que usa o Multer, onde os arquivos serão armazenados
-de acordo com a configuração definida em storage,
-*/
+const upload = multer({ storage: storage })
 
 module.exports = upload
