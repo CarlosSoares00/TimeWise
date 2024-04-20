47dde303bcb9906eaf1d1fdcdff89fd9c0cb23b2
CarlosSoares00 <carlossoarespedro20@gmail.com>
2024-04-20 22:11:27 +0100

    [Backend] Actualizando tudo e currigindo erros nos router

--- backend/src/routes/taskRouter.js
+++ backend/src/routes/taskRouter.js
@@ -6,8 +6,8 @@ const verifyToken = require('../middlewares/authMiddleware')
 
 router.post('/user/project/:idProject/task/create', verifyToken, taskController.createTask);
 router.get('/user/project/:idProject/task/getAll', verifyToken, taskController.getAllTasksByProject);
-router.get('/user/project/:idProject/task/:idTask', verifyToken, taskController.getTaskById);
-router.put('/user/project/:idProject/task/:idTask', verifyToken, taskController.editTask);
-router.delete('/user/project/:idProject/task/:idTask', verifyToken, taskController.deleteTask);
+router.get('/user/project/task/:idTask', verifyToken, taskController.getTaskById);
+router.put('/user/project/task/:idTask', verifyToken, taskController.editTask);
+router.delete('/user/project/task/:idTask', verifyToken, taskController.deleteTask);
 
 module.exports = router
